0e291d25cc833220de0f06da636944167c6a088a
Tiago Würthner <tiago.wuerthner@gmx.ch>
2026-01-17 13:01:33 -0300

    Add waybar switcher and default waybar as source

---
+++ bin/activate_block_waybar.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Activate block waybar theme
+cp ~/.config/omarchy/themes/tagruvu/gruvblock/config.jsonc  ~/.config/omarchy/themes/tagruvu/gruvblock/style.css ~/.local/share/omarchy/config/waybar/
---
+++ bin/activate_default_waybar.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Ativate default waybar theme
+cp ~/.config/omarchy/themes/tagruvu/def_waybar/config.jsonc  ~/.config/omarchy/themes/tagruvu/def_waybar/style.css ~/.local/share/omarchy/config/waybar/
